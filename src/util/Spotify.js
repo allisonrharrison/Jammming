478a6b882db543cbf5fd8faff86b63f07a8544ce
Allison Harrison <allisonrharrison@gmail.com>
2020-07-25 16:54:59 -0700

    Update Redirect URI

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = "7d182cb9c53a4e3e8c613d68dfb33199";
-const REDIRECT_URI = "http://special-instrument.surge.sh";
+const REDIRECT_URI = "http://arrogant-step.surge.sh";
 
 let accessToken;
 
