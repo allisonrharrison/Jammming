f68197015b924c1f0a76faa05051e9d7aec43b07
Allison Harrison <allisonrharrison@gmail.com>
2020-07-30 21:02:07 -0700

    Updates

---
+++ precache-manifest.b476b3d239efaa53eb5c6d7c2bbf06d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e4a838bc4ee9bf84f81f3049bbacab",
+    "url": "/Jammming/index.html"
+  },
+  {
+    "revision": "b6decc4e3ee28798fdc1",
+    "url": "/Jammming/static/css/main.ab03c90c.chunk.css"
+  },
+  {
+    "revision": "7c0adc6bac97871a6623",
+    "url": "/Jammming/static/js/2.2a8253dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jammming/static/js/2.2a8253dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6decc4e3ee28798fdc1",
+    "url": "/Jammming/static/js/main.ac0e267b.chunk.js"
+  },
+  {
+    "revision": "7e7a0a17cc65b1c77cbd",
+    "url": "/Jammming/static/js/runtime-main.c8a79d7b.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/Jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
